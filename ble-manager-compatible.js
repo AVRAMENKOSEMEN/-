e01df09febf78741d3e0c70676067d023992d9b7
Семён <61841515+AVRAMENKOSEMEN@users.noreply.github.com>
2025-10-17 01:08:51 +0600

    Update ble-manager-compatible.js

--- ble-manager-compatible.js
+++ ble-manager-compatible.js
@@ -17,6 +17,9 @@ class BLEManager {
             this.device = await navigator.bluetooth.requestDevice({
                 filters: [{ name: 'ESP32-Tracker' }],
                 optionalServices: ['12345678-1234-1234-1234-123456789abc']
+                acceptAllDevices: true,
+                optionalServices: ['12345678-1234-1234-1234-123456789abc']
+                optionalServices: ['12345678-1234-1234-1234-123456789abc','4fafc201-1fb5-459e-8fcc-c5c9c331914b']
             });
 
             console.log('📱 Устройство найдено:', this.device.name);
